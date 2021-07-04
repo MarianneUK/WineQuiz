0f98bdd435e47bad02945c77744a36f262f13f2e
Marianne J <marianne.johany@gmail.com>
2021-07-04 20:29:24 +0000

    Add quiz questions to html file instead

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const start_btn = document.querySelector(".start_btn button");
-const quiz_box = document.querySelector(".quiz_box");
-const result_box = document.querySelector(".result_box");
-const option_list = document.querySelector(".options");
-
-start_btn.onclick = ()=>{
-    quiz_box.classList.add("activeQuiz");
-}
